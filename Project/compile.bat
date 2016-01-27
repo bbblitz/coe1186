163fcf33a6d5c6271728d1fb42a1db98e2a485e8
Alexander Pickering <alexandermpickering@gmail.com>
2016-01-27 14:48:24 -0500

    Added compile and run scripts

--- Project/compile.bat
+++ Project/compile.bat
@@ -1,1 +1,8 @@
-javac -s ./src -d ./bin
+::remove any old files that were compiled before
+rmdir /S bin
+
+::And re-create the bin directory
+mkdir bin
+
+::Then compile everything, starting with main
+javac -s ./src -d ./bin main.java
---
+++ Project/run.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+java bin/main.class
