a8438aba635dd5e81d42044b5e38539d1b0023c1
Nick Faughey <nickfaughey@gmail.com>
2016-02-21 16:37:11 -0500

    General thoughts on dispatching a train

--- Project/src/CTC/DispatchPane.java
+++ Project/src/CTC/DispatchPane.java
@@ -5,7 +5,7 @@ import java.util.*;
 
 public class DispatchPane extends JPanel{
 
-  public DispatchPane(){
+  public DispatchPane() {
     JLabel l = new JLabel("Dispatch:");
     JLabel trainname = new JLabel("Train:");
     JTextField station = new JTextField();
@@ -15,5 +15,15 @@ public class DispatchPane extends JPanel{
     add(trainname);
     add(station);
     add(dispatchbutton);
+
+    // dispatch a dummy train
+    int id = 15;
+    BlockInterface currentBlock = config.aldl.get(0).line.get(0);    // the first block in the red line is right outside the yard
+    BlockInterface destination = config.aldl.get(0).line.get(7);    // go to the 7th block in the red line
+    long targetTime = System.currentTimeMillis() + 240000;  // 4 minutes from now
+    Route newRoute = new Route(destination, targetTime);
+    Train newTrain = new Train(id, currentBlock, newRoute);
   }
+
+
 }
--- Project/src/CTC/Train.java
+++ Project/src/CTC/Train.java
@@ -1,4 +1,12 @@
 public class Train {
 	public BlockInterface currentBlock;
 	public Route route;
+
+	public Train(int id, BlockInterface currentBlock, Route route) {
+		this.currentBlock = currentBlock;
+		this.route = route;
+
+		// create the actual train
+		TrainModel newTrain = new TrainModel(id);
+	}
 }
