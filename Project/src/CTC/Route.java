8eafd550e30ee01b9bcfd6db911512e20bb7bf1c
Nick Faughey <nickfaughey@gmail.com>
2016-02-21 16:29:11 -0500

    Framework for creating a route

--- Project/src/CTC/Route.java
+++ Project/src/CTC/Route.java
@@ -4,4 +4,13 @@ public class Route {
 	public ArrayList<BlockInterface> route;
 	public long targetTime;
 
+	public Route(BlockInterface destination, long targetTime) {
+		this.targetTime = targetTime;
+		this.route = calculateRoute(destination);
+	}
+
+	// calculate a list of blocks to travel through, given a destination
+	private ArrayList<BlockInterface> calculateRoute(BlockInterface destination) {
+		// do some magic to figure out a route from the yard to `destination`
+	}
 }
