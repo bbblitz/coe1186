6f99d5916507d453cc240d6fd1c958b9e36d0b5c
Nick Faughey <nickfaughey@gmail.com>
2016-02-22 13:15:57 -0500

    Added block type for station

---
+++ Project/src/CTC/BlockStation.java
@@ -0,0 +1,39 @@
+
+public class BlockStation extends BlockInterface{
+  public BlockInterface head;
+  public BlockInterface tail;
+  public BlockPart headto;
+  public BlockPart tailto;
+  public string name;
+
+  //How long the track is
+  public int length;
+
+  //The x offset from some origin
+  public int x;
+
+  //The y offset from some origin
+  public int y;
+
+  //The direction the track is faceing (in degrees)
+  public int direction;
+
+  public BlockStation(int tx, int ty, int dir, int length){
+    this.length = length;
+    x = tx;
+    y = ty;
+    direction = dir;
+    super.setFailState(TrackFailState.FS_NORMAL);
+  }
+
+  /**
+   *@override
+   */
+  public BlockInterface goesto(BlockInterface from){
+    if(from == head){
+      return tail;
+    }else{
+      return head;
+    }
+  }
+}
