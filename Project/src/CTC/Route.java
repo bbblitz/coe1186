8d3085a2f46297306158c9df75df3dd4a0f906e1
Nick Faughey <nickfaughey@gmail.com>
2016-02-24 19:51:40 -0500

    New Train and Route constructor

--- Project/src/CTC/Route.java
+++ Project/src/CTC/Route.java
@@ -5,14 +5,26 @@ public class Route {
 	public long targetTime;
 	public Config config;
 
-	public Route(Config config, BlockInterface destination, long targetTime) {
+	public Route(Config config, BlockInterface from, BlockInterface to, long targetTime) {
 		this.targetTime = targetTime;
 		this.route = calculateRoute(destination);
 		this.config = config;
 	}
-
-	// calculate a list of blocks to travel through, given a destination
+	
 	private ArrayList<BlockInterface> calculateRoute(BlockInterface destinationBlock) {
+		ArrayList<BlockInterface> route = new ArrayList<BlockInterface>();
+		
+		Line line = destinationBlock.getLine();
+		BlockInterface currentBlock = line.blocks.get(0);	// start at the first block (yard?)
+		
+		
+		
+		return route;
+	}
+
+	/*
+	// calculate a list of blocks to travel through, given a destination
+	private ArrayList<BlockInterface> calculateRoute2(BlockInterface destinationBlock) {
 		// do some magic to figure out a route from the yard to `destination`
 		ArrayList<BlockInterface> route = new ArrayList<BlockInterface>();
 		
@@ -35,4 +47,5 @@ public class Route {
 		
 		return route;
 	}
+	*/
 }
--- Project/src/CTC/Train.java
+++ Project/src/CTC/Train.java
@@ -2,11 +2,22 @@ public class Train {
 	public BlockInterface currentBlock;
 	public Route route;
 
-	public Train(int id, BlockInterface currentBlock, Route route) {
+	public Train(int id, BlockInterface currentBlock) {
 		this.currentBlock = currentBlock;
-		this.route = route;
 
 		// create the actual train
 		TrainModel newTrain = new TrainModel(/*id*/);
+	}
+	
+	public void setRoute(Route route) {
+		this.route = route;
+	}
+	
+	public Route getRoute() {
+		return this.route;
+	}
+	
+	public BlockInterface getBlock() {
+		return this.currentBlock;
 	}
 }
