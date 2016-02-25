429dee6e10da9374e11218cb87ae3ceae24ec3c3
Alexander Pickering <alexandermpickering@gmail.com>
2016-02-24 21:11:32 -0500

    More work on parser

--- Project/src/CTC/LineParser.java
+++ Project/src/CTC/LineParser.java
@@ -1,28 +1,36 @@
 
+import java.util.*;
+import java.io.*;
+
 public class LineParser{
   public ArrayList<Line> all = new ArrayList<Line>();
+  public Line curline;
   public ArrayList<Integer> heads = new ArrayList<Integer>();
   public ArrayList<Integer> tails = new ArrayList<Integer>();
+  public ArrayList<Integer> divergents = new ArrayList<Integer>();
   public Scanner s;
   public boolean incomment = false;
   public String fname;
-  public LineParser(String filename,int linenum,Config config){
+  public Config c;
+  public LineParser(String filename, Config config){
     fname = filename;
+    c = config;
     ArrayList<BlockInterface> newline = new ArrayList<BlockInterface>();
     File f = new File(filename);
     s = new Scanner(f);
     while(s.hasNextLine()){
-      String blockstring = s.getNextLine();
+      String blockstring = s.nextLine();
       System.out.println("Parseing: " + blockstring);
 
     }
     if(incomment){
       System.out.printf("Error: while parseing %s, hit end of file while looking for end of comment",fname);
+      return;
     }
-    config.aldl.set(linenum,newline);
+    //config.aldl.add(linenum,newline);
   }
 
-  public BlockPart parsePart(Strign p){
+  public BlockPart parsePart(String p){
     if(p.equals("HEAD")){
       return BlockPart.SEC_HEAD;
     }else if(p.equals("TAIL")){
@@ -32,22 +40,99 @@ public class LineParser{
     }
   }
 
-  public void parseLine(String s){
-    if(blockstring.sub(0,1).equals("#") || incomment){//Comment
+  public void resolveblocks(){
+    //Resolve all the pointers
+    //Now we need to hook all the blocks up
+    for(int i = 0; i < all.size(); i++){
+      BlockInterface block = curline.blocks.get(i);
+      if(block instanceof BlockStraight){
+        BlockStraight blockstr = (BlockStraight)block;
+        if(curline.blocks.get(heads.get(i)) == null){
+          System.out.printf("Error: While parseing %s, track section %d's head is connected to %d, but it dosen't exist!");
+          return;
+        }else{
+          blockstr.head = curline.blocks.get(heads.get(i));
+        }
+
+        if(curline.blocks.get(tails.get(i)) == null){
+          System.out.printf("Error: While parseing %s, track section %d's head is connected to %d, but it dosen't exist!");
+          return;
+        }else{
+          blockstr.tail = curline.blocks.get(tails.get(i));
+        }
+      }else if(block instanceof BlockCurved){
+        BlockCurved blockcur = (BlockCurved)block;
+        if(curline.blocks.get(heads.get(i)) == null){
+          System.out.printf("Error: While parseing %s, track section %d's head is connected to %d, but it dosen't exist!");
+          return;
+        }else{
+          blockcur.head = curline.blocks.get(heads.get(i));
+        }
+
+        if(curline.blocks.get(tails.get(i)) == null){
+          System.out.printf("Error: While parseing %s, track section %d's head is connected to %d, but it dosen't exist!");
+          return;
+        }else{
+          blockcur.tail = curline.blocks.get(tails.get(i));
+        }
+      }else if(block instanceof BlockSwitch){
+        BlockSwitch blockswi = (BlockSwitch)block;
+        if(curline.blocks.get(heads.get(i)) == null){
+          System.out.printf("Error: While parseing %s, track section %d's head is connected to %d, but it dosen't exist!");
+          return;
+        }else{
+          blockswi.head = curline.blocks.get(heads.get(i));
+        }
+
+        if(curline.blocks.get(tails.get(i)) == null){
+          System.out.printf("Error: While parseing %s, track section %d's head is connected to %d, but it dosen't exist!");
+          return;
+        }else{
+          blockswi.tail = curline.blocks.get(tails.get(i));
+        }
+
+        if(curline.blocks.get(divergents.get(i)) == null){
+          System.out.printf("Error: While parseing %s, track section %d's head is connected to %d, but it dosen't exist!");
+          return;
+        }else{
+          blockswi.divergent = curline.blocks.get(divergents.get(i));
+        }
+      }
+
+
+
+    }
+  }
+
+  public void parseLine(String blockstring){
+    if(blockstring.substring(0,1).equals("#") || incomment){//Comment
       return;
     }
-    if(blockstring.sub(0,2).equals("/*")){
+    if(blockstring.substring(0,2).equals("/*")){
       incomment = true;
       return;
-    }else if(blockstring.sub(0,2).equals("*/")){
+    }else if(blockstring.substring(0,2).equals("*/")){
       incomment = false;
       return;
     }
 
-    if(blockstring.sub(0,3).equals("str")){//This block should be straight
-      //Format: x, y, rotation, length, head, tail, headto, tailto
+    //Seperateing lines:
+    if(blockstring.substring(0,4).equals("LINE")){
+      String[] vs = blockstring.split(" ");
+      if(vs[1].equals("END")){
+        resolveblocks();
+        all.add(curline);
+        curline = null;
+      }else{
+        curline = new Line();
+        curline.name = vs[1];
+      }
+    }
+
+    if(blockstring.substring(0,3).equals("str")){//This block should be straight
+      //Format: x, y, rotation, length, head, tail, headto, tailto, id
       //  public BlockStraight(int tx, int ty, int dir, int length block_head, block_tail, headto, tailto){
-      String[] vs = s.split(",");
+      String[] vs = blockstring.split(",");
       int x = Integer.parseInt(vs[0]);
       int y = Integer.parseInt(vs[1]);
       int direction = Integer.parseInt(vs[2]);
@@ -56,40 +141,71 @@ public class LineParser{
       int tailid = Integer.parseInt(vs[5]);
       BlockPart headto = parsePart(vs[6]);
       BlockPart tailto = parsePart(vs[7]);
+      int blockid = Integer.parseInt(vs[8]);
       BlockStraight newblock = new BlockStraight(x,y,direction,length);
-      if(all.get(headid) != null){
+      heads.add(blockid, headid);
+      tails.add(blockid, tailid);
+      curline.blocks.add(blockid, newblock);
 
-      }
-      if(all.get(Integer.parseInt(vs[4])))
-      all.add(newblock);
-
-    }else if(blockstring.sub(0,3).equals("cur")){//This block should be curved
-      //Format: x, y, start_angle, angle_length, radius, block_clockwise, block_counterclockwise, block_clockwise_to, block_counterclockwise_to
+    }else if(blockstring.substring(0,3).equals("cur")){//This block should be curved
+      //Format: x, y, start_angle, angle_length, radius, block_clockwise, block_counterclockwise, block_clockwise_to, block_counterclockwise_to, block_id
       //public BlockCurved(int tx, int ty, int sa, int ea, int r){
-      String[] vs = s.split(",");
-      BlockCurved newblock = new BlockCurved(vs[0],vs[1],vs[2],vs[3], vs[4]);
+      String[] vs = blockstring.split(",");
+      int x = Integer.parseInt(vs[0]);
+      int y = Integer.parseInt(vs[1]);
+      int startang = Integer.parseInt(vs[2]);
+      int anglen = Integer.parseInt(vs[3]);
+      int radius = Integer.parseInt(vs[4]);
+      int headid = Integer.parseInt(vs[5]);
+      int tailid = Integer.parseInt(vs[6]);
+      BlockPart headto = parsePart(vs[7]);
+      BlockPart tailto = parsePart(vs[8]);
+      int blockid = Integer.parseInt(vs[9]);
+      BlockCurved newblock = new BlockCurved(x,y,startang,anglen,radius);
+      heads.add(blockid,headid);
+      tails.add(blockid,tailid);
 
-      all.add(newblock);
+      curline.blocks.add(newblock);
 
-    }else if(blockstring.sub(0,3).equals("swi")){//This block should be a switch
+    }else if(blockstring.substring(0,3).equals("swi")){//This block should be a switch
       System.out.println("Detected switch block");
+      String[] vs = blockstring.split(",");
+      int x = Integer.parseInt(vs[0]);
+      int y = Integer.parseInt(vs[1]);
+      int direction = Integer.parseInt(vs[2]);
+      int length = Integer.parseInt(vs[3]);
+      int headid = Integer.parseInt(vs[4]);
+      int tailid = Integer.parseInt(vs[5]);
+      int divergentid = Integer.parseInt(vs[6]);
+      BlockPart headto = parsePart(vs[7]);
+      BlockPart tailto = parsePart(vs[8]);
+      BlockPart divto = parsePart(vs[9]);
+      int blockid = Integer.parseInt(vs[10]);
+
+      BlockSwitch newblock = new BlockSwitch(x,y,direction,length);
+      newblock.headto = headto;
+      newblock.tailto = tailto;
+      newblock.divergentto = divto;
+      heads.add(blockid, headid);
+      tails.add(blockid, tailid);
+      divergents.add(blockid, divergentid);
       //Parse the next 3 parts
+      /*
       for(int i = 0; i < 3; i++){
         if(!s.hasNextLine()){
           System.out.printf("Error: While parseing %s, hit the end of file while expecting switch parts", fname);
           return;
         }
-        String headstring = parseLine(s.nextLine());
-        String divergentstring = parseLine(s.nextLine());
-        String tailstring = parseLine(s.nextLine());
-        BlockPart tail = all.get(all.size()-1);
-        BlockPart divergent = all.get(all.size()-2);
-        BlockPart head = all.get(all.size()-3);
+        parseLine(s.nextLine());
+        parseLine(s.nextLine());
+        parseLine(s.nextLine());
+        BlockInterface tail = curline.blocks.get(curline.blocks.size()-1);
+        BlockInterface divergent = curline.blocks.get(curline.blocks.size()-2);
+        BlockInterface head = curline.blocks.get(curline.blocks.size()-3);
         //Format:
         //public BlockSwitch(int tx, int ty, int d, int l){
         //Followed by 3 parts that make up the switch
-        BlockSwitch newblock = new BlockSwitch()
-
+        */
       }
     }
   }
--- Project/src/CTC/main.java
+++ Project/src/CTC/main.java
@@ -19,6 +19,8 @@ public class main{
     window.setSize(config.windowDim.width,config.windowDim.height);
     config.window = window;
 
+    ArrayList<Line> aldl = new ArrayList<Line>();
+    LineParser parser = new LineParser("track.txt",config);
     /*
     LineOne dlo = new LineOne();
     ArrayList<Line> aldl = new ArrayList<Line>();
