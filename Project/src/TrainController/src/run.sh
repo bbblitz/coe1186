6ae4e6421b8156b1e6cf669f893a83797537e179
Nick Faughey <nickfaughey@gmail.com>
2016-04-04 16:09:06 -0400

    cd into folder to run

--- Project/src/TrainController/src/run.sh
+++ Project/src/TrainController/src/run.sh
@@ -1,11 +1,20 @@
 #/bin/bash
 
+
 mkdir bin
 
+# copy dependencies into ./bin
 cp *.java ./bin
+cp ../../TrainModel/*.java ./bin
 
-javac Main.java -d ./bin
+cd ./bin
 
-java -classpath ./bin Main
+# compile
+javac Main.java
 
+# run
+java Main
+
+# clean up
+cd ..
 rm -rf ./bin
