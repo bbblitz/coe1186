049b5fb2152110435e0ceaf15d14971a57528432
Nick Faughey <nickfaughey@gmail.com>
2016-04-21 13:06:36 -0400

    Move test files out of working directory

---
+++ Project/src/TrainController/src/test/Beacon.java
@@ -0,0 +1,11 @@
+public class Beacon {
+
+    String stationName;
+    int distanceToStationEnd;
+
+    public Beacon(String stationName, int distanceToStationEnd) {
+        this.stationName = stationName;
+        this.distanceToStationEnd = distanceToStationEnd;
+    }
+
+}
